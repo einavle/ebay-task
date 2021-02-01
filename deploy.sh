d5ec472991cb37817e7bcde63b9915693ea0d17b
i515295 <e.leyboshor@sap.com>
2021-02-01 23:40:28 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 docker compose up &
 RET_CODE=0
 
-while [ ${RET_CODE} != 200 ]; do
+while [[ ${RET_CODE} != "200" ]]; do
  sleep 3
  RET_CODE=$(curl -I http://admin:admin@localhost:3000/api/dashboards 2>/dev/null | head -n 1 | cut -d$' ' -f2)
 done

dee1d147ddd62e840b3cfa8d564762dd6d6afa32
i515295 <e.leyboshor@sap.com>
2021-02-05 22:48:35 +0200

    fix curl command

--- upload-panel.sh
+++ upload-panel.sh
@@ -2,4 +2,4 @@
 curl --header "Content-Type: application/json" \
   --request POST \
   --data @panel.json \
-  http://admin:admin@http://34.69.204.54:3000/api/dashboards/db
+  http://admin:admin@34.69.204.54:3000/api/dashboards/db

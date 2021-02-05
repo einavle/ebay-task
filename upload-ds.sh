8a0a635aba08f60b6d7d671165b6860150579989
einavle <e.leyboshor@sap.com>
2021-02-05 21:58:38 +0200

    Update upload-ds.sh

--- upload-ds.sh
+++ upload-ds.sh
@@ -2,4 +2,4 @@
 curl --header "Content-Type: application/json" \
   --request POST \
   --data @datasource.json \
-  http://admin:admin@localhost:3000/api/datasources
+  http://admin:admin@http://34.69.204.54:3000/api/datasources
